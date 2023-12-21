b3c3097d6aa971f7c10a1e45865d39c198fd4db8
Risdan Kristori <75466526+Risdan224@users.noreply.github.com>
2023-12-21 08:44:04 +0700

    Update predict.py

--- code/predict.py
+++ code/predict.py
@@ -25,7 +25,7 @@ from sklearn.compose import TransformedTargetRegressor
 def predict(df):
     # Load the model and scaler from the saved file
     # Load model
-    file_name =str(Path(__file__).parents[1] / 'model/indonesia_used_car_colab_v2.pkl')
+    file_name =str(Path(__file__).parents[1] / 'model/indonesia_used_car_colab_v3.pkl')
     model = pickle.load(open(file_name, 'rb'))
     
     pred = model.predict(df)
